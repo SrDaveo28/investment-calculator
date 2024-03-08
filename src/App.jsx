cfeafc4b0dc1d397bf30d4ad91c7cabe152c10c7
SrDaveo28 <davortizmds@gmail.com>
2024-03-07 22:49:08 -0300

    FEAT[results & userInput]: adding new components and functionability to calculate the user investment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
 function App() {
-  return <Header />;
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevuserInput) => {
+      return {
+        ...prevuserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  };
+
+  return (
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greather than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+      {/*  */}
+    </>
+  );
 }
 
 export default App;
